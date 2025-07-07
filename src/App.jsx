3ea4288c7c84b6b10ec39e5455cd4e501c30deb5
Sujoy Karmakar <sujoykr8@gmail.com>
2025-07-07 07:03:27 +0600

    add get call tmdb

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ const App = () => {
         <div className="pattern"/>
         <div className="wrapper">
             <header>
+                <img src="./hero.png" alt="Hero Thumbnail"/>
                 <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle.</h1>
             </header>
             <p>Search</p>
